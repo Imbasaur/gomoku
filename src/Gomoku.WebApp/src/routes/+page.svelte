b19324e692a74302e95a7c67568b27d0b80d9e43
Jarek Godlewski <yarrusg@gmail.com>
2024-03-05 23:10:53 +0100

    add gomoku page

--- src/Gomoku.WebApp/src/routes/+page.svelte
+++ src/Gomoku.WebApp/src/routes/+page.svelte
@@ -20,7 +20,7 @@
 
 		to your new<br />SvelteKit app
 	</h1>
-
+	
 	<h2>
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
--- src/Gomoku.WebApp/src/routes/Header.svelte
+++ src/Gomoku.WebApp/src/routes/Header.svelte
@@ -25,6 +25,9 @@
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
 			</li>
+			<li aria-current={$page.url.pathname.startsWith('/board') ? 'page' : undefined}>
+				<a href="/board">Gomoku board</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
@@ -32,7 +35,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/Imbasaur/gomoku">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
