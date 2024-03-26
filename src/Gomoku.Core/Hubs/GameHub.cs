4aaa4973cf285514a8eb8f262b36b014adced0c7
Jarek Godlewski <yarrusg@gmail.com>
2024-03-26 23:56:14 +0100

    fix signalr not sending msgs back

--- src/Gomoku.Core/Hubs/GameHub.cs
+++ src/Gomoku.Core/Hubs/GameHub.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Gomoku.Core.Hubs;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 
 namespace Gomoku.Api.Hubs;
 
-public class GameHub(ILogger<GameHub> logger) : Hub
+public class GameHub(ILogger<GameHub> logger) : Hub<IGameHub>
 {
     public async Task SendMessageAsync(string name, string message)
     {
-        await Clients.All.SendAsync(name, message);
+        await Clients.All.SendMessageAsync(name, message);
     }
 
     public override Task OnConnectedAsync()
--- src/Gomoku.Core/Services/WaitingListService.cs
+++ src/Gomoku.Core/Services/WaitingListService.cs
@@ -1,19 +1,20 @@
 ﻿using AutoMapper;
 using Gomoku.Api.Hubs;
 using Gomoku.Core.Dtos.WaitingList;
+using Gomoku.Core.Hubs;
 using Gomoku.Core.Services.Abstract;
 using Gomoku.DAL.Entities;
 using Gomoku.DAL.Repository;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Gomoku.Core.Services;
-public class WaitingListService(IWaitingListRepository repository, IMapper mapper, IHubContext<GameHub> hub, IGameService gameService) : IWaitingListService
+public class WaitingListService(IWaitingListRepository repository, IMapper mapper, IHubContext<GameHub, IGameHub> hub, IGameService gameService) : IWaitingListService
 {
     public async Task Add(string playerName)
     {
         repository.Add(new PlayerWaiting(playerName));
 
-        await hub.Clients.All.SendAsync("PlayerJoinedWaitingList", playerName); // remove later, no need to send this to front
+        await hub.Clients.All.SendMessageAsync("PlayerJoinedWaitingList", playerName); // remove later, no need to send this to front
 
         if (repository.Count() > 1)
             await gameService.Create();
@@ -26,18 +27,19 @@ public class WaitingListService(IWaitingListRepository repository, IMapper mappe
         return Task.FromResult(repository.Count());
     }
 
-    public Task<IEnumerable<PlayerWaitingDto>> GetAll()
+    public async Task<IEnumerable<PlayerWaitingDto>> GetAll()
     {
         var players = repository.GetMany();
+        await hub.Clients.All.SendMessageAsync("PlayersInQueue", players.ToString());
 
-        return Task.FromResult(mapper.Map<IEnumerable<PlayerWaitingDto>>(players));
+        return mapper.Map<IEnumerable<PlayerWaitingDto>>(players);
     }
 
     public async Task Remove(string playerName)
     {
         repository.Delete(x => x.PlayerName.ToLower().Equals(playerName.ToLower())); // this will be id in future
 
-        await hub.Clients.All.SendAsync("PlayerLeftWaitingList", playerName);
+        await hub.Clients.All.SendMessageAsync("PlayerLeftWaitingList", playerName);
 
         return;
     }
