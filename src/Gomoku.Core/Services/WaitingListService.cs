dd849cdcabff53caa2731567f82e424c2ca4ba64
Jarek Godlewski <yarrusg@gmail.com>
2024-04-03 20:18:44 +0200

    fix get all waitinglist

--- src/Gomoku.Core/Services/WaitingListService.cs
+++ src/Gomoku.Core/Services/WaitingListService.cs
@@ -5,6 +5,7 @@ using Gomoku.Core.Services.Abstract;
 using Gomoku.DAL.Entities;
 using Gomoku.DAL.Repository;
 using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
 
 namespace Gomoku.Core.Services;
 public class WaitingListService(IWaitingListRepository repository, IMapper mapper, IHubContext<GameHub> hub, IGameService gameService) : IWaitingListService
@@ -28,8 +29,8 @@ public class WaitingListService(IWaitingListRepository repository, IMapper mappe
 
     public async Task<IEnumerable<PlayerWaitingDto>> GetAll()
     {
-        var players = repository.GetManyAsync();
-        await hub.Clients.All.SendAsync("PlayersInQueue", players.ToString());
+        var players = await repository.GetManyAsync();
+        await hub.Clients.All.SendAsync("PlayersInQueue", JsonSerializer.Serialize(players.Select(x => x.PlayerName)));
 
         return mapper.Map<IEnumerable<PlayerWaitingDto>>(players);
     }
