174341fb8b80d12827f62bda5e5510c3f09ebd4e
Jarek Godlewski <yarrusg@gmail.com>
2024-04-19 23:45:38 +0200

    add game board css

--- src/Gomoku.WebApp/src/routes/game/+page.svelte
+++ src/Gomoku.WebApp/src/routes/game/+page.svelte
@@ -17,10 +17,11 @@
 	
 
 <div id="board">
-	{#each Array.from(Array(15).keys()) as row (row)}
-		<div class="row{row}">
-			{#each Array.from(Array(15).keys()) as column (column)}
-				<div class="node" id="node{row}{column}"></div>
+	{#each Array.from(Array(15).keys()) as column (column)}
+		<div class="column{column}">
+			{#each Array.from(Array(15).keys()) as row (row)}
+
+				<div class="node column-{column} row-{row}" id="node{column}{row}"></div>
 			{/each}
 		</div>
 	{/each}
@@ -43,26 +44,185 @@
 </div>
 
 <style>
-	#board {
-		margin-left: auto;
-		margin-right: auto;
-		width: 600px;
-		height: 600px;
-		display: grid;
-		grid-template-columns: repeat(15, 0fr);
+    #board {
+        margin-left: auto;
+        margin-right: auto;
+        width: 600px;
+        height: 600px;
+        display: grid;
+        grid-template-columns: repeat(15, 0fr);
+    }
+
+    .node {
+        width: 30px;
+        height: 30px;
+        position: relative; /* Add position relative */
+        background-color: #ECB163;
+        font-size: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    /* Horizontal line */
+    .node::after {
+        content: '';
+        position: absolute;
+        top: 15px; /* Position in the middle vertically */
+        left: 0;
+        width: 100%;
+        height: 1px; /* Height of the line */
+        background-color: #000000;
+    }
+
+    /* Vertical line */
+    .node::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 15px; /* Position in the middle horizontally */
+        width: 1px; /* Width of the line */
+        height: 100%;
+        background-color: #000000;
+    }
+
+	/* Left border */ 
+	.column-0::after{
+		content: '';
+		position: absolute;
+		top: 15px;
+		left: 15px;
+		width: 1px;
+		height: 50%;
+		background-color: #000000;
+	}
+	.column-0::after{
+			left: 15px;
+			top: 15px;
+			width: 50%;
+			height: 1px;
 	}
 
-	.node {
-		width: 30px;
-		height: 30px;
-		border: 1px solid #000000;
-		background-color: #ECB163;
-		font-size: 40px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
+	/* Right border */ 
+	.column-14::before{
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 15px;
+		width: 1px;
+		height: 100%;
+		background-color: #000000;
+	}
+	.column-14::after{
+		left: 0;
+		top: 15px;
+		width: 50%;
+		height: 1px;
+	}
+
+	/* Top border */ 
+	.row-0::before{
+		content: '';
+		position: absolute;
+		top: 15px;
+		left: 15px;
+		width: 1px;
+		height: 50%;
+		background-color: #000000;
+	}
+	.row-0::after{
+			left: 0;
+			top: 15px;
+			width: 100%;
+			height: 1px;
+	}
+
+	/* Bottom border */ 
+	.row-14::before{
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 15px;
+		width: 1px;
+		height: 50%;
+		background-color: #000000;
+	}
+	.row-14::after{
+		left: 0;
+		top: 15px;
+		width: 100%;
+		height: 1px;
+	}
+
+    /* Top left corner */
+	#node00::before,
+	#node00::after {
+		content: '';
+		position: absolute;
+		top: 15px;
+		left: 15px;
+		width: 1px;
+		height: 50%;
+		background-color: #000000;
+	}
+	#node00::after {
+		left: 15px;
+		top: 15px;
+		width: 50%;
+		height: 1px;
+	}
+
+	/* Bottom left corner */
+	#node014::before,
+	#node014::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 15px;
+		width: 1px;
+		height: 50%;
+		background-color: #000000;
+	}
+	#node014::after {
+		left: 15px;
+		top: 15px;
+		width: 50%;
+		height: 1px;
+	}
+
+	/* Top right corner */
+	#node140::before,
+	#node140::after {
+		content: '';
+		position: absolute;
+		top: 15px;
+		left: 15px;
+		width: 1px;
+		height: 50%;
+		background-color: #000000;
+	}
+	#node140::after {
+		left: 0;
+		top: 15px;
+		width: 50%;
+		height: 1px;
+	}
+
+	/* Top right corner */
+	#node1414::before,
+	#node1414::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 15px;
+		width: 1px;
+		height: 50%;
+		background-color: #000000;
+	}
+	#node1414::after {
+		left: 0;
+		top: 15px;
+		width: 50%;
+		height: 1px;
 	}
 </style>
-
-
-
