ebcc49f1688fc120565e9cb725450b010d1c1871
Jarek Godlewski <yarrusg@gmail.com>
2024-05-23 23:41:51 +0200

    cleanup

--- src/Gomoku.WebApp/src/routes/+page.svelte
+++ src/Gomoku.WebApp/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
@@ -24,8 +23,6 @@
 	<h2>
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
-
-	<Counter />
 </section>
 
 <style>
--- src/Gomoku.WebApp/src/routes/Header.svelte
+++ src/Gomoku.WebApp/src/routes/Header.svelte
@@ -19,12 +19,6 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
 			<li aria-current={$page.url.pathname.startsWith('/game') ? 'page' : undefined}>
 				<a href="/game">Gomoku</a>
 			</li>
